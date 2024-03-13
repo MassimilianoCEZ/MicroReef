ecef60748b6a1e72808b255cf78c9c65c02301ad
Agent004CJ <55920551+Agent004CJ@users.noreply.github.com>
2024-03-13 16:16:35 +0100

    updated .vscode file

--- shape.cc
+++ shape.cc
@@ -1,4 +1,7 @@
+#include <iostream>  //only preliminary REMOVE AFTER TEST
+using namespace std; // only preliminary REMOVE AFTER TEST
 
+constexpr double epsil_zero(0.5);
 
 struct S2d // (plus robuste au bugs; according to "specification" page 10/12)
 {
@@ -6,7 +9,11 @@ struct S2d // (plus robuste au bugs; according to "specification" page 10/12)
     double y = 0.;
 };
 
-//pleliminary main function to test code
-int main() {
-    S2d;
+
+// pleliminary main function to test code
+int main()
+{
+    S2d testPoint;
+    cout << testPoint.x << " is x and" << endl;
+    cout << testPoint.y << " is y";
 }

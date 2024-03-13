0876a9b35c654e59bf27dd1880192e750cd87943
Agent004CJ <55920551+Agent004CJ@users.noreply.github.com>
2024-03-13 16:59:00 +0100

    added comment

--- shape.cc
+++ shape.cc
@@ -1,5 +1,6 @@
 #include "shape.h"
 
+// ASK DAniel for wether to use struc or vector??? if yes how do we define it?? i dont understand the "specifications"
 struct S2d // (plus robuste au bugs; according to "specification" page 10/12 rather than using class)
 {
     double x = 0.;
--- testShape.cc
+++ testShape.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "shape.h" // NOTE this Massimo ;)
+#include "shape.h" // NOTE this Massi ;)
                     // how to make sure that whatever is on this file has acces to shape.cc
 using namespace std;
 int main()

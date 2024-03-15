3678a03699d17d9e4b8ac5b3450fe698a6342728
Agent004CJ <55920551+Agent004CJ@users.noreply.github.com>
2024-03-15 14:16:25 +0100

    nothing in particular

--- shape.cc
+++ shape.cc
@@ -6,11 +6,23 @@ struct S2d // (better against bugs; according to "specification" page 10/12 rath
     double y = 0.;
 };
 
-class Segment{
+class Segment
+{
 private:
     S2d base;
     double angle;
-    double lenght;
+    double length;
 
 public:
+    Segment(S2d baseIpt, double angleIpt = 0.0, double lengthIpt = 0.0)
+        : base(), angle(angleIpt), length(lengthIpt) // can you put a struct in the initialization list? base???
+    {
+    }
+
+    const double getAngle()
+    {
+        return angle;
+    }
 };
+
+
